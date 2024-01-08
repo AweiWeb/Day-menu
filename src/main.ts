cd3c539a4978c03e7e063c9a8f2bb4706f804ffe
王超伟 <>
2024-01-08 18:42:30 +0800

    element-plus自动导入

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
-import './style/index.css'
+import './style/index.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,36 @@
 <script lang="ts" setup></script>
 <template>
-  <div>login</div>
+  <div class="login">
+    <div class="loginBox">
+      <h1 class="title">每日菜单</h1>
+      <input type="text" />
+      <input type="text" />
+      <button>登录</button>
+    </div>
+  </div>
 </template>
-<style scoped></style>
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url('../../assets/login.jpg');
+  background-size: 100% 100%;
+  width: 100vw;
+  height: 100vh;
+
+  .loginBox {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .title {
+      color: #d1bbff;
+    }
+
+    input {
+      margin-bottom: 1vh;
+    }
+  }
+}
+</style>
