2f0ab4e85452cf7b2fa26b34dddbb91fc4fa9b72
王超伟 <>
2024-01-15 11:24:21 +0800

    layout布局的搭建路由的配置

---
+++ src/views/Order/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="menu-order">订单页面</div>
+</template>
+<style lang="scss"></style>
---
+++ src/views/Recommend/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="menu-recommend">推荐页面</div>
+</template>
+<style lang="scss"></style>
