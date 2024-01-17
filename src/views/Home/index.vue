8bdda0bf015aeee0960f51827a3a95b085e5486c
廖哲勤 <liaozheqin@dingtalk.com>
2024-01-17 21:52:09 +0800

    测试

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup></script>
 <template>
   <!-- 测试大量数据页面滚动效果 -->
-  <div v-for="item in 100" :key="item">Home</div>
+  <div v-for="item in 100" :key="item">Home1</div>
 </template>
 <style scoped></style>
